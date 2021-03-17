ab4f56cbf9c899a1f39c5be8362f46d8ea865e02
Saheed Ogunsolu <saheedogunsolu2016@gmail.com>
2021-03-17 11:45:09 +0100

    adding severless

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const fetch = require("node-fetch");
 
 export default async (req, res) => {
   console.log("req", req.method);
